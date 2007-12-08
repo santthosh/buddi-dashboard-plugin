08c509391db968ed1f9857f644b345975fc6f183
santthosh <santthosh@df54d750-0f3f-0410-8789-cde074e7ce19>
2007-12-08 05:08:58 +0000

    Added sample JFreeChart component to panel

--- buddi-dashboard-plugin/Dashboard/src/org/homeunix/thecave/plugins/Dashboard/ChartPanel.java
+++ buddi-dashboard-plugin/Dashboard/src/org/homeunix/thecave/plugins/Dashboard/ChartPanel.java
@@ -5,9 +5,19 @@ package org.homeunix.thecave.plugins.dashboard;
 
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+import java.awt.image.BufferedImage;
+
 
 import org.homeunix.thecave.moss.swing.MossFrame;
 import org.homeunix.thecave.moss.swing.MossPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.plot.PlotOrientation;
 
 
 /**
@@ -17,7 +27,8 @@ import org.homeunix.thecave.moss.swing.MossPanel;
 public class ChartPanel extends MossPanel {
 	public static final long serialVersionUID = 0;
 	
-	private final MossFrame parent;     	 
+	private final MossFrame parent;     
+	JLabel lblChart;
 	
 	public ChartPanel(MossFrame parent){
 		super(true);
@@ -28,18 +39,33 @@ public class ChartPanel extends MossPanel {
 	}
 		
 	public void init() {	
-		super.init();
-		
-		setBorder(BorderFactory.createLineBorder(new java.awt.Color(0, 102, 153)));
-		GroupLayout layout = new GroupLayout(this);
-		this.setLayout(layout);
-		layout.setHorizontalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addGap(0, 350, Short.MAX_VALUE)
-		);
-		layout.setVerticalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addGap(0, 100, Short.MAX_VALUE)
-		);
+		super.init();		
+		setBorder(BorderFactory.createLineBorder(new java.awt.Color(0, 102, 153)));		
+		paintChart();
    	}
+	
+	public void paintChart()
+	{
+		DefaultCategoryDataset categoryDataset = new DefaultCategoryDataset();
+		categoryDataset.addValue(5D, "Dinner","Food");
+		categoryDataset.addValue(10.2D, "Lunch","Food");
+		categoryDataset.addValue(11.3D, "BreakFast","Food");
+		categoryDataset.addValue(15.4D, "Soap","Groceries");
+		categoryDataset.addValue(10.4D, "Detergent","Groceries");
+		JFreeChart chart = ChartFactory.createBarChart(				
+				"", 
+				"Category", 
+				"Percentage", 
+				categoryDataset, 
+				PlotOrientation.VERTICAL,
+				false,
+				false,
+				false);		                    
+						
+		BufferedImage image = chart.createBufferedImage(500,150);
+		lblChart = new JLabel();
+		lblChart.setIcon(new ImageIcon(image));	
+		
+		this.add(lblChart);
+	}
 }
